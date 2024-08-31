ede1577fba73f7efd9262417885b7f3e77911a0c
dilumin <77558016+dilumin@users.noreply.github.com>
2024-08-31 22:32:28 +0530

    Navigate to chat from wall added

--- client/src/pages/Home/components/Navbar.jsx
+++ client/src/pages/Home/components/Navbar.jsx
@@ -2,11 +2,15 @@ import React, { useState, useEffect, useRef } from 'react';
 import HomeContext from '../../../context/HomeProvider';
 import { useContext } from 'react';
 import './Navbar.css';
+import chat from './chat-svgrepo-com (1).svg';
+import { useNavigate } from 'react-router-dom';
 
 function Navbar() {
   const [search, setSearch] = useState('');
-  const { profileDropDown, setProfileDropDown, MyInfo, friends } = useContext(HomeContext);
-  
+  const { profileDropDown, setProfileDropDown, MyInfo, friends } =
+    useContext(HomeContext);
+  const navigate = useNavigate();
+
   const profileRef = useRef(null);
 
   useEffect(() => {
@@ -24,7 +28,8 @@ function Navbar() {
 
   return (
     <div>
-      <div className='navbar-con w-full bg-slate-300 h-16 flex items-center justify-between px-4'>
+      <div className="navbar-con w-full bg-slate-300 h-16 flex items-center justify-between px-4">
+        <div className="logo text-black font-bold text-xl">PhotoNia</div>
         <div className="flex-1 flex justify-center">
           <div className="input-con w-2/5 bg-white rounded-lg">
             <input
@@ -36,21 +41,35 @@ function Navbar() {
           </div>
         </div>
         <div className="profile-con flex items-center relative">
+          <div className="">
+            <img
+              onClick={() => {
+                navigate('/dashboard');
+              }}
+              src={chat}
+              alt=""
+              className="w-6 h-6 cursor-pointer mr-6"
+            />
+          </div>
           <div
-            className='profile bg-black w-10 h-10 rounded-full border border-black cursor-pointer'
+            className="profile bg-black w-10 h-10 rounded-full border border-black cursor-pointer"
             onClick={() => setProfileDropDown(!profileDropDown)}
           ></div>
         </div>
       </div>
-      {profileDropDown &&
+      {profileDropDown && (
         <div
           ref={profileRef} // Attach the ref to the dropdown
-          className='fixed right-0 border-solid bg-slate-500 border-black rounded w-24 p-1'
+          className="fixed right-0 border-solid bg-slate-500 border-black rounded w-24 p-1"
         >
-          <div className='chec border-black cursor-pointer p-2 text-white'>Profile</div>
-          <div className='chec border-black cursor-pointer p-2 text-white'>Sign Out</div>
+          <div className="chec border-black cursor-pointer p-2 text-white">
+            Profile
+          </div>
+          <div className="chec border-black cursor-pointer p-2 text-white">
+            Sign Out
+          </div>
         </div>
-      }
+      )}
     </div>
   );
 }
