5b0d62a2eee0cc7231aee9da6c8c0264b3a3e328
dilumin <77558016+dilumin@users.noreply.github.com>
2024-09-09 22:45:48 +0530

    TSsti

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -4,91 +4,103 @@ import {
   MDBInput,
   MDBCheckbox,
   MDBBtn,
-  MDBIcon
-}
-from 'mdb-react-ui-kit';
-import { Navigate, useNavigate , useLocation } from 'react-router-dom';
-import AuthContext from '../../context/AuthProvider'
+  MDBIcon,
+} from 'mdb-react-ui-kit';
+import { Navigate, useNavigate, useLocation } from 'react-router-dom';
+import AuthContext from '../../context/AuthProvider';
 import { useContext, useEffect } from 'react';
-
 
 import { useState } from 'react';
 
 function Login() {
+  const { Auth, setAuth } = useContext(AuthContext);
 
-    
-    const {Auth , setAuth} = useContext(AuthContext);
-
-    useEffect(() => {
-        if (Auth.accessToken) {
-            navigate('/dashboard');
-        }} , [Auth] );
-    
-
-    const goToReg = () => {
-        navigate('/register');
+  useEffect(() => {
+    if (Auth.accessToken) {
+      navigate('/dashboard');
     }
+  }, [Auth]);
 
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate();
-    const location = useLocation();
-    
+  const goToReg = () => {
+    navigate('/register');
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const location = useLocation();
 
-        try{
-            const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
-            // const response = await fetch('https://testserver-arap.onrender.com/login', {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`${process.env.REACT_APP_API_URL}/login`, {
+        // const response = await fetch('https://testserver-arap.onrender.com/login', {
 
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                credentials: 'include', 
-                body: JSON.stringify({email, password})
-            });
-            const data = await response.json();
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+        body: JSON.stringify({ email, password }),
+      });
+      const data = await response.json();
 
-            if (data.Status === "Success") {
-                setAuth({accessToken : data.AccessToken});
-                localStorage.setItem('accessToken', data.AccessToken);
-                navigate(  location.state?.from || { pathname: '/Dashboard' } , { replace: true });
-            }
-            else{
-                alert("User login failed" , data?.message)
-                console.log(data);
-                console.log("User login failed");
-            }
-        } catch (err) {
-            console.error("Error while POSTING in handle submit ",err);      
-        }
+      if (data.Status === 'Success') {
+        setAuth({ accessToken: data.AccessToken });
+        localStorage.setItem('accessToken', data.AccessToken);
+        navigate(location.state?.from || { pathname: '/Dashboard' }, {
+          replace: true,
+        });
+      } else {
+        alert('User login failed', data?.message);
+        console.log(data);
+        console.log('User login failed');
+      }
+    } catch (err) {
+      console.error('Error while POSTING in handle submit ', err);
     }
-
+  };
 
   return (
     <div className="flex items-center justify-center h-screen">
-        <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
+      <MDBContainer className="p-3 my-5 d-flex flex-column w-50">
         <form onSubmit={handleSubmit}>
-            <MDBInput onChange={(e) => setEmail(e.target.value)} wrapperClass='mb-4' label='Email address' id='form1' type='text'/>
-            <MDBInput onChange={(e) => setPassword(e.target.value)} wrapperClass='mb-4' label='Password' id='form2' type='password'/>
+          <MDBInput
+            onChange={(e) => setEmail(e.target.value)}
+            wrapperClass="mb-4"
+            label="Email address"
+            id="form1"
+            type="text"
+          />
+          <MDBInput
+            onChange={(e) => setPassword(e.target.value)}
+            wrapperClass="mb-4"
+            label="Password"
+            id="form2"
+            type="password"
+          />
 
-            <div className="d-flex justify-content-between mx-3 mb-4">
-                <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-                <a href="!#">Forgot password?</a>
-            </div>
+          <div className="d-flex justify-content-between mx-3 mb-4">
+            <MDBCheckbox
+              name="flexCheck"
+              value=""
+              id="flexCheckDefault"
+              label="Remember me"
+            />
+            <a href="!#">Forgot password?</a>
+          </div>
 
-            <MDBBtn type='submit' className="mb-4">Sign in</MDBBtn>
+          <MDBBtn type="submit" className="mb-4">
+            Sign in
+          </MDBBtn>
         </form>
 
         <div className="text-center">
-            <p>Not a member? <button onClick={goToReg} >Register</button></p>
-            
+          <p>
+            Not a member? <button onClick={goToReg}>Register</button>
+          </p>
         </div>
-
-        
-        </MDBContainer>
+      </MDBContainer>
     </div>
   );
 }
